9d0ebcad86ca774569015007473d089e2b00f796
Logan Compton <lcompto@iu.edu>
2018-07-10 16:02:34 -0400

    Add GithubUser component with params

--- src/App.js
+++ src/App.js
@@ -1,21 +1,33 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { Route, NavLink, Switch } from 'react-router-dom'
+
+import './App.css'
+import Github from './Github'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+        <header>
+          <h3>Ain't no party like an</h3>
+          <h1>API Party</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        
+        <ul className="navLinks">
+          <li>
+            <NavLink to="/github">GitHub API</NavLink>
+          </li>
+        </ul>
+
+        <Switch>
+          <Route path="/github" render={() => <h1>GitHub!</h1>} />
+          <Route render={() => (
+            <p>To get started, click one of the links above.</p>
+          )}/>
+        </Switch>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
